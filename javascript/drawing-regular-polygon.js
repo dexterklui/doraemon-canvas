f1d3c786e63830c1f00651f4de71903e953dfaf4
kelseywong88 <midnightoil20210724@gmail.com>
2023-08-05 17:28:30 +0800

    fixing the rotation problem

---
+++ javascript/drawing-regular-polygon.js
@@ -0,0 +1,68 @@
+class DrawingRegularPolygon extends PaintFunction {
+    constructor(contextReal, contextDraft) {
+      super(contextReal, contextDraft);
+    }
+  
+    onMouseDown(coord, event) {
+      this.origX = coord[0];
+      this.origY = coord[1];
+    }
+  
+    onDragging(coord, event) {
+      this.clearDraft();
+      let sides = parseInt(document.getElementById("side").value);
+      let angle = 360 - Math.atan2(this.origY - coord[1], this.origX - coord[0]) * 180 / Math.PI;
+      let coordinates = [],
+          radius = Math.sqrt(Math.pow((coord[0] - this.origX), 2) + Math.pow((coord[1] - this.origY), 2)),
+          index = 0;
+  
+       for (index; index < sides; index++) {
+          coordinates.push({
+              x: this.origX + radius * Math.cos(angle),
+              y: this.origY - radius * Math.sin(angle)
+          })
+          angle += (2 * Math.PI) / sides;
+      }
+  
+      this.contextDraft.beginPath();
+      this.contextDraft.moveTo(coordinates[0].x, coordinates[0].y);
+  
+      for (index = 0; index < sides; index++) {
+          this.contextDraft.lineTo(coordinates[index].x, coordinates[index].y);
+      }
+  
+      this.contextDraft.closePath();
+      this.contextDraft.stroke();
+    }
+  
+    onMouseMove() { }
+  
+    onMouseUp(coord) {
+      this.clearDraft();
+      let sides = parseInt(document.getElementById("side").value);
+      let angle = 360 - Math.atan2(this.origY - coord[1], this.origX - coord[0]) * 180 / Math.PI;
+      let coordinates = [],
+          radius = Math.sqrt(Math.pow((coord[0] - this.origX), 2) + Math.pow((coord[1] - this.origY), 2)),
+          index = 0;
+  
+       for (index; index < sides; index++) {
+          coordinates.push({
+              x: this.origX + radius * Math.cos(angle),
+              y: this.origY - radius * Math.sin(angle)
+          })
+          angle += (2 * Math.PI) / sides;
+      }
+  
+      this.contextReal.beginPath();
+      this.contextReal.moveTo(coordinates[0].x, coordinates[0].y);
+      for (index = 0; index < sides; index++) {
+          this.contextReal.lineTo(coordinates[index].x, coordinates[index].y);
+      }
+  
+      this.contextReal.closePath();
+      this.contextReal.stroke();
+    }
+    onMouseLeave() { }
+    onMouseEnter() { }
+  }
+  
